ec2c4be8899371e9f7f8169033f5e6a05c7508c0
shivshankar.dayal@gmail.com <shivshankar.dayal@gmail.com>
2013-07-09 03:33:12 +0530

    added Flask-OpenID in install-packages as it was missing

--- install-packages.sh
+++ install-packages.sh
@@ -2,4 +2,4 @@
 
 virtualenv venv
 source venv/bin/activate
-pip install --upgrade Flask Flask-WTF Flask-Bcrypt Flask-Gravatar Flask-Login couchbase itsdangerous Flask-mail
+pip install --upgrade Flask Flask-WTF Flask-Bcrypt Flask-Gravatar Flask-Login couchbase itsdangerous Flask-mail Flask-OpenID
