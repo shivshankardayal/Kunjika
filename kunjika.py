b003847ac7fe775d510e72165ffa854fe453df8e
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2013-07-15 03:32:53 +0530

    bad perl regex

--- kunjika.py
+++ kunjika.py
@@ -6,7 +6,7 @@ from flaskext.bcrypt import Bcrypt
 from couchbase import Couchbase
 from couchbase.exceptions import *
 import urllib2
-from p#print import pprint
+from pprint import pprint
 from flaskext.gravatar import Gravatar
 from werkzeug import secure_filename, SharedDataMiddleware
 import os
