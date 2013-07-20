825666af4bf1c54ff15d2887fd676535ffc9f91b
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2013-07-20 20:45:17 +0530

    added license to source code. need to add to templates and documentation.

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright (c) 2013 Shiv Shankar Dayal
+# This file is part of Kunjika.
+#
+# Kunjika is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Kunjika is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
 virtualenv venv
 source venv/bin/activate
 pip install --upgrade Flask Flask-WTF Flask-Bcrypt Flask-Gravatar Flask-Login couchbase itsdangerous Flask-mail Flask-OpenID
