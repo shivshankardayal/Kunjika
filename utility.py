4a1ae1d94fecadbeea74042c3bb6fbe04a4c2b96
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2014-06-26 15:31:46 +0530

    fixing bugs for tags

--- utility.py
+++ utility.py
@@ -461,8 +461,8 @@ def get_questions_for_tag(page, QUESTIONS_PER_PAGE, tag):
     skip = (page - 1) * QUESTIONS_PER_PAGE
     tag = urllib2.quote(tag, '')
     rows = urllib2.urlopen(kunjika.DB_URL + 'questions/_design/dev_qa/_view/get_qid_from_tag?limit=' +
-                str(QUESTIONS_PER_PAGE) + '&skip=' + str(skip) + '&key="' + urllib.quote(tag) + '"&reduce=false').read()
-    count = urllib2.urlopen(kunjika.DB_URL + 'questions/_design/dev_qa/_view/get_qid_from_tag?key=' + '"' + urllib.quote(tag) + '"&reduce=true').read()
+                str(QUESTIONS_PER_PAGE) + '&skip=' + str(skip) + '&key="' + tag + '"&reduce=false').read()
+    count = urllib2.urlopen(kunjika.DB_URL + 'questions/_design/dev_qa/_view/get_qid_from_tag?key=' + '"' + tag + '"&reduce=true').read()
     count = json.loads(count)['rows'][0]['value']
     #tag = kunjika.tb.get(tag).value
     rows = json.loads(rows)['rows']
